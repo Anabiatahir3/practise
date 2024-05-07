1cbfcd849cedb03b8388aa327d2f98a4969f75b6
Anabiatahir3 <anabiatahir365@gmail.com>
2024-05-07 11:20:56 +0500

    app without context

---
+++ src/dummy_projects.jsx
@@ -0,0 +1,19 @@
+import Blue_Tshirt from "./assets/Blue_Tshirt.jpg";
+export const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    image: Blue_Tshirt,
+    title: "Majestic Vintage Mocha Overcoat",
+    price: 129.99,
+    description:
+      "Channel timeless sophistication with this stunning mocha overcoat. Crafted for the discerning gentleman who appreciates the fine blend of vintage charm and modern elegance.",
+  },
+  {
+    id: "p2",
+    image: Blue_Tshirt,
+    title: "Enchanting Blush Dream Gown",
+    price: 189.99,
+    description:
+      "Bask in the glow of elegance with our Enchanting Blush Dream Gown. Embody the regality of a queen with a sweet touch of pink that whispers enchantment. This is the perfect piece for those seeking to create unforgettable moments.",
+  },
+];
