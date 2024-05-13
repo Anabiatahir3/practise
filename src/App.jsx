9ab30e3ed5c391615ac4bf6c904053f29576f2c5
Anabiatahir3 <anabiatahir365@gmail.com>
2024-05-13 11:21:24 +0500

    small custom hook example

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Quiz from "./components/Quiz";
 import QuestionTimer from "./components/QuestionTimer";
-
+import useNetwork from "./hooks/useNetwork";
 function App() {
+  const isOnline = useNetwork();
+  console.log("val", isOnline);
   return (
     <>
       <Header />
-      <main>
-        <Quiz />
-      </main>
+      <main>{isOnline ? <Quiz /> : "No internet"}</main>
     </>
   );
 }
