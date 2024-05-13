2f8a28458410e0354ddbfc6fe9e5a12af20cb6a7
Anabiatahir3 <anabiatahir365@gmail.com>
2024-05-13 11:38:26 +0500

    initial form files

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import Player from "./components/Player";
-import { useState } from "react";
-import TimerChallenge from "./components/TimerChallenge";
+import Header from "./components/Header";
+import Login from "./components/Login";
 
 function App() {
-  
   return (
-
     <>
-      <Player  />
-      <div id="challenges">
-        <TimerChallenge title={"Easy"} targetTime={1}/>
-        <TimerChallenge title={"Medium"} targetTime={2}/>
-        <TimerChallenge title={"Expert"} targetTime={3}/>
-        <TimerChallenge title={"Pro"} targetTime={4}/>
-
-
-
-      </div>
-    
+      <Header />
+      <Login />
     </>
   );
 }
