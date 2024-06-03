dc9696396ed1d40438cc07c0d52d0d8faa6ff103
Anabiatahir3 <anabiatahir365@gmail.com>
2024-06-03 10:37:32 +0500

    interview qs practise

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,47 @@
-import Player from "./components/Player";
-import { useState } from "react";
-import TimerChallenge from "./components/TimerChallenge";
+import { useState, useEffect } from "react";
+const useDebounce = (text, delay) => {
+  const [debounce, setDebounce] = useState(text);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebounce(text);
+    }, delay);
 
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [text, delay]);
+
+  return debounce;
+};
+
+const FRUITS = ["Apple", "apricot", "banana", "orange"];
 function App() {
-  
+  const [entered, setEntered] = useState("");
+  const [filtered, setFiltered] = useState([]);
+  const debounceText = useDebounce(entered, 1000);
+
+  useEffect(() => {
+    const filteredFruits = FRUITS.filter((item) =>
+      item.toLowerCase().includes(debounceText.toLowerCase())
+    );
+    setFiltered(filteredFruits);
+  }, [debounceText]);
+
+  const handleInput = (e) => {
+    const input = e.target.value;
+    setEntered(input);
+  };
   return (
-
-    <>
-      <Player  />
-      <div id="challenges">
-        <TimerChallenge title={"Easy"} targetTime={1}/>
-        <TimerChallenge title={"Medium"} targetTime={2}/>
-        <TimerChallenge title={"Expert"} targetTime={3}/>
-        <TimerChallenge title={"Pro"} targetTime={4}/>
-
-
-
-      </div>
-    
-    </>
+    <div>
+      <input type="text" value={entered} onChange={handleInput} />
+      {filtered.length > 0 && (
+        <ul>
+          {filtered.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
 
